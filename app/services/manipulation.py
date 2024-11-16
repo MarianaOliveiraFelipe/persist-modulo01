b9153b061b1ad9710857e1d1e8c23526eaa042f7
Yagho <yaghoms3@gmail.com>
2024-11-16 05:24:52 -0300

    F2/json, F5/zip, refatoração

--- app/services/manipulation.py
+++ app/services/manipulation.py
@@ -10,17 +10,15 @@ def adicionar_exercicio(exercicio: Exercicio) -> Exercicio:
     if any(e.id == exercicio.id for e in exercicios):
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="ID já existe.")
     exercicios.append(exercicio)
-    salvar_exercicios(exercicios)  # Chama salvar_exercicio ao invés de salvar_exercicios
+    salvar_exercicios(exercicios)
     return exercicio
 
 def atualizar_exercicio(exercicio_id: int, exercicio_atualizado: Exercicio) -> Exercicio:
     for indice, exercicio in enumerate(exercicios):
         if exercicio.id == exercicio_id:
-            # Substituindo o exercício existente
             exercicios[indice] = exercicio_atualizado
-            salvar_exercicios(exercicios)  # Salva as atualizações no arquivo CSV
+            salvar_exercicios(exercicios)
             return exercicio_atualizado
-    # Se o exercício com o ID não for encontrado
     raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Exercício não encontrado.")
 
 def remover_exercicio(exercicio_id: int):
